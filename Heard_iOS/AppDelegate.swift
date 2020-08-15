c2a0b8e6e6ac9e720593c0f42943cab3e17c5998
talharahman <talharahman@pursuit.org>
2020-08-14 23:01:56 -0400

    add login UI and sign in

--- Heard_iOS/AppDelegate.swift
+++ Heard_iOS/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         FirebaseApp.configure()
+        let db = Firestore.firestore()
+        print(db)
         
         return true
     }
--- Heard_iOS/Controller/LoginViewController.swift
+++ Heard_iOS/Controller/LoginViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        signInButton.layer.cornerRadius =
+            signInButton.frame.size.height / 5
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInPressed(_ sender: UIButton) {
+        if let email = emailTextField.text,
+            let password = passwordTextField.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) {
+                 authResult, error in
+                 if let e = error {
+                 print(e.localizedDescription)
+                 } else {
+                     self.performSegue(withIdentifier: "ActionLoginToUser", sender: self)
+                 }
+            }
+            
+        }
+    }
+    
 }
 
